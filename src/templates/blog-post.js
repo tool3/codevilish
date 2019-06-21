a0eeb9e310744295d6e473c2d720ae4be210ec5d
Tal Hayut <tal.hayut@zerto.com>
2019-06-21 23:04:10 +0300

    Styled theming

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,18 +5,30 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
-import styled from 'styled-components';
+import styled, { ThemeProvider } from "styled-components"
+import theme from "styled-theming"
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
-    const { tags } = post.frontmatter;
+    const { tags } = post.frontmatter
+
+
+    const backgroundColor = theme("mode", {
+      light: "#fff", dark: "#000",
+    })
+
+    const color = theme("mode", {
+      light: "#000", dark: "#fff",
+    })
+
 
     const Tag = styled.div`
-      background: gray;
-      color: white;
+      background-color: ${backgroundColor};
+      color: ${color};
+      border: 1px solid ${color};
       border-radius: 13px;
       padding: 5px;
       text-align: center;
@@ -24,52 +36,57 @@ class BlogPostTemplate extends React.Component {
       font-size: 15px;
       line-height: 12px;
       margin: 5px;
-    `;
+    `
 
     const TagWrapper = styled.div`
       display: flex;
       justify-content: flex-start;
     `
+    console.log(backgroundColor)
 
-    return (<Layout location={this.props.location} title={siteTitle}>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
-      <h1>{post.frontmatter.title}</h1>
-      <p style={{
-        ...scale(-1 / 5),
-        display: `block`,
-        marginBottom: rhythm(1),
-        marginTop: rhythm(-1),
-      }}>
-        {post.frontmatter.date}
-      </p>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}/>
-      <hr style={{ marginBottom: rhythm(1) }}/>
-      {tags && <TagWrapper>{tags.map(tag => <Tag>{tag}</Tag>)}</TagWrapper>}
-      <Bio/>
-      <ul
-        style={{
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-          listStyle: `none`,
-          padding: 0,
-        }}
-      >
-        <li>
-          {previous && (<Link to={previous.fields.slug} rel="prev">
-            ← {previous.frontmatter.title}
-          </Link>)}
-        </li>
-        <li>
-          {next && (<Link to={next.fields.slug} rel="next">
-            {next.frontmatter.title} →
-          </Link>)}
-        </li>
-      </ul>
-    </Layout>)
+    return (<ThemeProvider theme={{ mode: "dark" }}>
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <h1>{post.frontmatter.title}</h1>
+        <p style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}>
+          {post.frontmatter.date}
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}/>
+        <hr style={{ marginBottom: rhythm(1) }}/>
+
+        {tags && <TagWrapper>{tags.map(tag => <Tag>{tag}</Tag>)}</TagWrapper>}
+
+        <Bio/>
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (<Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>)}
+          </li>
+          <li>
+            {next && (<Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>)}
+          </li>
+        </ul>
+      </Layout>
+    </ThemeProvider>)
   }
 }
 
