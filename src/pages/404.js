7c1bdd234e17a920ab1a95eb61fa623d5bc70b81
Tal Hayut <tal.hayut@zerto.com>
2019-07-14 17:02:55 +0300

    cool 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
 import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import GatsbyImage from "gatsby-image"
+import { StyledLink } from "../components/components"
+
+const NotFoundWrapper = styled.div`
+  text-align: center;
+`
+const NotFound = styled.div`
+  font-size: 10em;
+`
+const NotFoundMessage = styled.div`
+  font-size: 3em;
+`
 
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <>
-        <SEO title="404: Not Found" />
-        <h1>Page Not Found</h1>
-      </>
+      <NotFoundWrapper>
+        <SEO title="404: Not Found"/>
+        <NotFound>
+          4<GatsbyImage fixed={this.props.data.astonished.childImageSharp.fixed} alt="horns"/>4
+        </NotFound>
+        <NotFoundMessage>I know right ?!</NotFoundMessage>
+        <StyledLink to={"/"}>go back home</StyledLink>
+      </NotFoundWrapper>
     )
   }
 }
@@ -16,11 +33,18 @@ class NotFoundPage extends React.Component {
 export default NotFoundPage
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
+    query {
+        astonished : file(absolutePath: { regex: "/astonished.png/" }) {
+            childImageSharp {
+                fixed(width: 120, height: 120) {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        }
+        site {
+            siteMetadata {
+                title
+            }
+        }
     }
-  }
 `
