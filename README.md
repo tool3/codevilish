9687aa3448a7667f108608929de47efbfab48f87
Tal Hayut <tal.hayut@zerto.com>
2019-07-15 07:37:16 +0300

    config updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# CodeXS
+# Codevilish
 Hey ya'll this my personal blog where I present some coding tips, tricks and all around fun 🚀🔥 
+
+Feel free to fork it and change the content ! 🤘🏼
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -8,25 +8,30 @@ module.exports = {
       gitlab: "talhayut", github: "tool3", linkedIn: "talhayut",
     },
   }, plugins: [{
-    resolve: `gatsby-source-filesystem`, options: {
+    resolve: `gatsby-source-filesystem`,
+    options: {
       path: `${__dirname}/content/blog`, name: `blog`,
     },
   }, {
-    resolve: `gatsby-source-filesystem`, options: {
+    resolve: `gatsby-source-filesystem`,
+    options: {
       path: `${__dirname}/content/assets`, name: `assets`,
     },
   }, {
-    resolve: `gatsby-transformer-remark`, options: {
+    resolve: `gatsby-transformer-remark`,
+    options: {
       plugins: ["gatsby-remark-code-titles", {
         resolve: `gatsby-remark-prismjs`,
-        options: { showLineNumbers: true, show_names: true , copy: true},
-        plugins: 'prismjs/plugins/show-language/prism-show-language'
+        options: { showLineNumbers: true, show_names: true, copy: true },
+        plugins: "prismjs/plugins/show-language/prism-show-language",
       }, { resolve: "gatsby-remark-autolink-headers" }, {
-        resolve: `gatsby-remark-images`, options: {
+        resolve: `gatsby-remark-images`,
+        options: {
           maxWidth: 590,
         },
       }, {
-        resolve: `gatsby-remark-responsive-iframe`, options: {
+        resolve: `gatsby-remark-responsive-iframe`,
+        options: {
           wrapperStyle: `margin-bottom: 1.0725rem`,
         },
       }, `gatsby-remark-copy-linked-files`, `gatsby-remark-smartypants`,
@@ -34,11 +39,13 @@ module.exports = {
       ],
     },
   }, `gatsby-transformer-sharp`, `gatsby-plugin-sharp`, {
-    resolve: `gatsby-plugin-google-analytics`, options: {
-      //trackingId: `ADD YOUR TRACKING ID HERE`,
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-143755590-1",
     },
   }, `gatsby-plugin-feed`, {
-    resolve: `gatsby-plugin-manifest`, options: {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
       name: `Codevilish`,
       short_name: `Codevilish`,
       start_url: `/`,
@@ -48,7 +55,8 @@ module.exports = {
       display: `standalone`,
     },
   }, `gatsby-plugin-offline`, `gatsby-plugin-react-helmet`, {
-    resolve: `gatsby-plugin-typography`, options: {
+    resolve: `gatsby-plugin-typography`,
+    options: {
       pathToConfigModule: `src/utils/typography`,
     },
   }, `gatsby-plugin-styled-components`, {
