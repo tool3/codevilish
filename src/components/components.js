dafd82af3aabd56102f2be79daa1deabbbe21802
Tal Hayut <tal.hayut@zerto.com>
2019-07-16 09:58:41 +0300

    fixing stuff

--- src/components/components.js
+++ src/components/components.js
@@ -28,7 +28,7 @@ export const Wrapper = styled.div`
   background-color: ${backgroundColor};
   color: ${color};
   min-height: 100vh;
-  padding: 0 34em;
+  padding: 0 37em;
   
   @media only screen and (max-width: 1400px) {
         padding: 5px 11vw;
