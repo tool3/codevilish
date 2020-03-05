b485d2ca65820594f0d137ac2c153fd1f3c8455f
Tal Hayut <talhayut.dev@gmail.com>
2020-03-05 07:34:27 +0200

    fix: laptop viewport and padding

--- src/components/components.js
+++ src/components/components.js
@@ -27,12 +27,11 @@ export const Wrapper = styled.div`
   justify-content: space-between;
   background-color: ${backgroundColor};
   color: ${color};
-  min-height: 100%;
-  padding: 0 33em;
+  min-height: 100vh;
+  padding: 0 25em;
 
   @media only screen and (max-width: 1400px) {
     padding: 5px 11vw;
-    min-height: 100vh;
   }
 
   @media only screen and (max-width: 450px) {
